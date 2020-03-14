1e2ad4cd744e3c53368b5014f7bd444fba8d0bc6
emir <emir.jumaliev@gmail.com>
2020-03-13 23:49:41 -0400

    Добавлен media_root и  media_url, сделаны модели

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+class File(models.Model):
+    file = models.FileField(upload_to='uploads/study_crm/study_crm_homework_issues',null=False,blank=False,
+                                verbose_name='Задание')
+    name = models.CharField(max_length=2500, null=False, blank=False, verbose_name='Описание')
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='file_author',
+                               null=False, blank=True, verbose_name='Автор')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
 
 # Create your models here.
